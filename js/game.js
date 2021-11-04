fa34ce6a63351036c0249e2637ed7299df7d6dd4
FA1312 <faldrovandi83@gmail.com>
2021-11-04 21:29:46 +0100

    enemy animation implemented

--- js/game.js
+++ js/game.js
@@ -1,26 +1,8 @@
+// Getting canvas as a variable and defined context
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
-let enemies = [];
-let bullets = [];
 
-let mouse = {
-  x: canvas.width/2,
-  y: canvas.height/2
-}
-
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-  console.log(mouse)
-});
-
-addEventListener('click', shoot);
-addEventListener('keypress', (event) => {
-  if(event.code == "Space") {
-    shoot();
-  }
-})
-
+// Class Player
 class Player {
   constructor(x, y, radius, color) {
     this.x = x
@@ -30,61 +12,80 @@ class Player {
   }
   draw () {
     ctx.beginPath()
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    /*The arc() method creates a circular arc centered at (x, y) with a radius of radius. The path starts at startAngle, ends at endAngle, and travels in the direction given by counterclockwise (defaulting to clockwise).*/
+    /*The beginPath() method begins a path*/
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2)
+        /*The arc() method creates a circular arc centered at (x, y) with a radius of radius. The path starts at startAngle, ends at endAngle, and travels in the direction given by counterclockwise (defaulting to clockwise).*/
     ctx.fillStyle = this.color
-    ctx.fill()
-    ctx.closePath()
+    /*The CanvasRenderingContext2D.fillStyle property of the Canvas 2D API specifies the color, gradient, or pattern to use inside shapes. */
+    ctx.fill()    
   }
   update(){
     this.draw();
   }
 }
 
-function shoot() {
-  let bullet = new Player(player.x, player.y, 15, 'white')
-  let velocityx = mouse.x - bullet.x
-  let velocityy = mouse.y - bullet.y
-  let speed = 6
-  let distance = Math.sqrt(velocityy * velocityy + velocityx + velocityx)
-  bullet.dx = velocityx / distance
-  bullet.dy = velocityy / distance
-  bullet.dx *= speed;
-  bullet.dy *= speed;
-  bullets.push(bullet);
+//class Enemy
+class Enemy {
+  constructor(x, y, radius, color, speed){
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.speed = speed
+  }
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+  update() {
+    this.draw()
+  }
+  move() {
+    setInterval(()=>{
+    this.x += 1
+    this.y += 1
+    // encontrar la formula para que los enemigos se vayan hacia player
+  }, 1500)
+  }
 }
 
+//Initialize variables
 let player;
+let enemies = [];
+let bullets = []; // crear funcion shoot dentro de class Player o crear class Bullet y que maniobre como objeto solitario?
+
 
 function start() {
-  player = new Player(canvas.width/2, canvas.height, 30, 'blue')
+  //paint my player and given features
+  player = new Player(canvas.width/2, canvas.height, 40, 'red')
+}
+
+function spawnEnemies() {
+  setInterval(()=>{
+    let x = Math.random() * canvas.width /* Problema 1 los enemigos aparecen en punto random del mapa, yo quiero que esten al borde del canvas */
+    let y = Math.random() * canvas.height
+    const radius = 20
+    const color = "green"
+    const speed = 10 // Ni idea?!?!? crear distancia y que se vaya quedando mas cerca???
+    enemies.push(new Enemy(x, y, radius, color, speed))
+  },1500)
 }
 
 function update() {
   requestAnimationFrame(update);
+  //window.requestAnimationFrame(callback)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  // Bullets
-  for (let i = 0; i < bullets.length; i++) {
-    let bullet = bullets[i];
-
-    bullet.x += bullet.dx;
-    bullet.y += bullet.dy;
-
-    if (
-      bullet.x < 0 ||
-      bullet.x > canvas.width ||
-      bullet.y < 0 ||
-      bullet.y > canvas.height
-    ) {
-      bullets.splice(i, 1);
-      console.log(bullets);
-    }
-
-    bullet.update();
+  //clear the screen
+  player.update()
+  for (var i = 0; i < enemies.length; i++){
+    enemies[i].update()
+    enemies[i].move() 
   }
-  //Enemies
-   player.update()
+  //enemies.update is not a function solucionado iterando dentro del array de enemies
 }
 
+spawnEnemies()
 start()
 update()
